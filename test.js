0209fc036885166573290a8983d6acaf31b60a08
andresshare <andresshare@gmail.com>
2019-07-05 11:56:07 -0500

    code2

--- test.js
+++ test.js
@@ -7,4 +7,21 @@ for (let i = 0; i < 7; i++) {
     console.log(stringEmpty);
 }
 
-//
+//CODE2
+function fizzBuzz() {
+  for (i = 1; i <= 100; i++) {
+    if ((i % 3 === 0) && (i % 5 === 0)) {
+      console.log("FizzBuzz");
+    } else if ((i % 3 !== 0) && (i % 5 === 0)) {
+      console.log("Buzz");
+    } else if ((i % 3 === 0) && (i % 5 !== 0)) {
+      console.log("Fizz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+
+console.log(fizzBuzz())
+
